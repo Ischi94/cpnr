896ce956386aeaa346c1e17f09d4ebe9d53d23cc
Ischi94 <gmathes94@gmx.de>
2020-10-12 19:32:08 +0200

    add start tutorial

---
+++ R/start_tutorial.R
@@ -0,0 +1,48 @@
+#' Run a tutorial
+#'
+#' Run a specified tutorial in the cpnr package.
+#' A web browser will be started automatically to view the document.
+#'
+#' @inheritParams show_description
+#' @param shiny_args Additional arguments to forward to
+#' \code{\link[shiny::runApp]{runApp}}
+#'
+#' @examples
+#' \dontrun{
+#' start_tutorial(tutorial = "first_tutorial")
+#' }
+#'
+#'
+#' @rdname start_tutorial
+#'
+#' @details Note that, when finished, the running tutorial needs to be stopped
+#' by clicking on the stop symbol in the R console.
+#'
+#' @export
+start_tutorial <- function( tutorial, package = "cpnr", shiny_args = NULL){
+  tutorials_dir <- system.file("tutorials", package = package)
+
+  tutorial_folders <- list.dirs(tutorials_dir, full.names = TRUE,
+                                recursive = FALSE)
+
+  dir_rmd_files <- dir(tutorial_folders, pattern = "\\.Rmd$", recursive = FALSE,
+                       full.names = TRUE)
+
+  if (is.null(shiny_args))
+    shiny_args <- list()
+
+  if (is.null(shiny_args$launch.browser)) {
+    shiny_args$launch.browser <-
+      (interactive() || identical(Sys.getenv("LEARNR_INTERACTIVE", "0"), "1"))
+  }
+
+  withr::with_dir(tutorials_dir, {
+    if (!identical(Sys.getenv("SHINY_PORT", ""), "")) {
+      withr::local_envvar(c(RMARKDOWN_RUN_PRERENDER = "0"))
+    }
+    rmarkdown::run(file = NULL, dir = tutorial_folders, shiny_args = shiny_args,
+                   default_file = paste0(tutorial, ".Rmd"))
+  })
+}
+
+
