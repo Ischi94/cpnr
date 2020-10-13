2cf63545e206b7f4e8814bd877a1d0aaef09a1af
Ischi94 <gmathes94@gmx.de>
2020-10-13 11:14:24 +0200

    finnish load_dependencies

--- R/load_dependencies.R
+++ R/load_dependencies.R
@@ -1,3 +1,24 @@
+#' Load tutorial dependencies
+#'
+#' Load the package dependencies of the specified tutorial in the cpnr package.
+#' If the package dependency is not installed, this function will install and
+#' subsequently load it.
+#'
+#' @param packages Packages needed to run a tutorial
+#'
+#'
+#' @examples
+#' \dontrun{
+#' packages <- show_dependencies(tutorial = "first_tutorial")
+#' load_dependencies(packages = packages)
+#' }
+#'
+#' @seealso \code{show_dependencies} to get the tutorial dependencies.
+#'
+#'
+#' @rdname load_dependencies
+#'
+#' @export
 load_dependencies <- function(packages) {
   suppressWarnings(
   for(p in packages){
