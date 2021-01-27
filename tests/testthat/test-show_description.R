d5e1914762772a17fe8f9260cd5e764a3f75f34f
Ischi94 <gmathes94@gmx.de>
2021-01-27 08:54:18 +0100

    add show description test

---
+++ tests/testthat/test-show_description.R
@@ -0,0 +1,31 @@
+test_that("a string is returned", {
+  expect_true(is.character(show_description("divdyn")))
+})
+
+
+test_that("no matching tutorial warning is returned", {
+  expect_match(show_description("empty"), "No tutorial found with name: empty")
+})
+
+
+test_that("the correct tutorial description is returned", {
+
+  tutorials_dir <- system.file("tutorials", package = "cpnr")
+
+  tutorial_folders <- list.dirs(tutorials_dir, full.names = TRUE,
+                                recursive = FALSE)
+
+  dir_rmd_files <- dir(tutorial_folders, pattern = "\\.Rmd$", recursive = FALSE,
+                       full.names = TRUE)
+
+  yaml_front_matter <- data.frame(title = character(length(dir_rmd_files)),
+                                  description = character(length(dir_rmd_files)))
+
+  divdyn_yaml <- dir_rmd_files[grep("divdyn", dir_rmd_files)]
+
+  yaml_all <- rmarkdown::yaml_front_matter(divdyn_yaml)
+
+  yaml_description <- yaml_all$description
+
+  expect_match(show_description("divdyn"), yaml_description)
+})
